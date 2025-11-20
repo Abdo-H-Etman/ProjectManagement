14f4b74eb6221645861f1d0e99e1184ebe6969b0
Abdo Othman <abdoetman010@gmail.com>
2025-11-20 13:41:03 +0200

    [ADD] ConfigureDatabase as a service extension

---
+++ API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+using System;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureDatabase(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContextPool<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    
+}
