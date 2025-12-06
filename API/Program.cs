c32009f595998571d4a0de7d24ec98cc319c76c3
Abdo Othman <abdoetman010@gmail.com>
2025-12-06 13:29:26 +0200

    [ADD] implementation for dependency injection

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Extensions;
 using DotNetEnv;
+using Infrastructure.Utilities;
 
 // Load environment variables from .env before creating the WebApplicationBuilder
 // so they become part of the application's configuration sources.
@@ -8,6 +9,7 @@ Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureDatabase(builder.Configuration);
+builder.Services.AddRepositories();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
