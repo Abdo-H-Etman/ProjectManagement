feb0c1ec613e3e94435baff996d7dcfad9c53790
Abdo Othman <abdoetman010@gmail.com>
2025-12-27 21:32:03 +0200

    [ADD] implementation for logging sevice

---
+++ Core/Application/Services/Logging/LoggerManager.cs
@@ -0,0 +1,29 @@
+using System;
+using Application.Interfaces.Logging;
+using NLog;
+namespace Application.Services.Logging;
+
+public class LoggerManager : ILoggerManager
+{
+    private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+    public void LogInfo(string message)
+    {
+        _logger.Info(message);
+    }
+
+    public void LogWarn(string message)
+    {
+        _logger.Warn(message);
+    }
+
+    public void LogDebug(string message)
+    {
+        _logger.Debug(message);
+    }
+
+    public void LogError(string message)
+    {
+        _logger.Error(message);
+    }
+}
