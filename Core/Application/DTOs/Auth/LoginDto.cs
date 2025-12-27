de224ad29f8424974d69f56a6d6ee0a8339ce97f
Abdo Othman <abdoetman010@gmail.com>
2025-12-27 21:25:28 +0200

    [EDIT] change the property name from UserName to Identifier for best practice

--- Core/Application/DTOs/Auth/LoginDto.cs
+++ Core/Application/DTOs/Auth/LoginDto.cs
@@ -2,6 +2,6 @@ namespace Application.DTOs.Auth;
 
 public record LoginDto
 {
-    public string UserName { get; init; } = null!;
+    public string Identifier { get; init; } = null!;
     public string Password { get; init; } = null!;
 }
