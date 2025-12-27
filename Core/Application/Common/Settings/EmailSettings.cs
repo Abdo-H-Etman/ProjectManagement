f4be3f74dfc916e98c35a6e16e31d0d1542085cd
Abdo Othman <abdoetman010@gmail.com>
2025-12-27 21:31:12 +0200

    [ADD] EmailSettings and JwtSettings models for configuration management

---
+++ Core/Application/Common/Settings/EmailSettings.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Application.Common.Settings;
+
+public class EmailSettings
+{
+    public string SmtpServer { get; set; } = string.Empty;
+    public int SmtpPort { get; set; }
+    public string SenderName { get; set; } = string.Empty;
+    public string SenderEmail { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public bool EnableSsl { get; set; } = true;
+    public bool UseDefaultCredentials { get; set; } = false;
+}
