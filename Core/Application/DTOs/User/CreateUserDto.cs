0a28d367a5f63b80614a024961bac4b38ea4af00
Abdo Othman <abdoetman010@gmail.com>
2025-12-27 21:24:18 +0200

    [ADD] ConfirmPassword to user creation DTO (CreateUserDto)

--- Core/Application/DTOs/User/CreateUserDto.cs
+++ Core/Application/DTOs/User/CreateUserDto.cs
@@ -7,4 +7,5 @@ public record CreateUserDto
     public string Email { get; init; } = null!;
     public string UserName { get; init; } = null!;
     public string Password { get; init; } = null!;
+    public string ConfirmPassword { get; init; } = null!;
 }
