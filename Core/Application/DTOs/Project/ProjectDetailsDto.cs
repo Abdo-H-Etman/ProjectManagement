515b241ee19e2f23ee0feedca8525ebfa36de6e5
Abdo Othman <abdoetman010@gmail.com>
2026-01-07 14:26:06 +0200

    [ADD] include Members, Tags, and Attachments properties in ProjectDetailsDto

--- Core/Application/DTOs/Project/ProjectDetailsDto.cs
+++ Core/Application/DTOs/Project/ProjectDetailsDto.cs
@@ -1,8 +1,14 @@
+using Application.DTOs.Attachment;
 using Application.DTOs.Task;
+using Application.DTOs.User;
+using Domain.Entities.Models;
 
 namespace Application.DTOs.Project;
 
 public record ProjectDetailsDto : ProjectListDto
 {
     public IEnumerable<TaskSummaryDto> Tasks { get; init; } = [];
+    public IEnumerable<UserDto> Members { get; init; } = [];
+    public IEnumerable<string> Tags { get; init; } = [];
+    public IEnumerable<AttachmentDto> Attachments { get; init; } = [];
 }
