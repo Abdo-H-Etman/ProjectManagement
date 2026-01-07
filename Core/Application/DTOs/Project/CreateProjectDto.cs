70624c70420546ad12e3936484d6da12b50052e2
Abdo Othman <abdoetman010@gmail.com>
2026-01-07 14:24:13 +0200

    [EDIT] change the type of Visibility to make its easier to pass in the request body

--- Core/Application/DTOs/Project/CreateProjectDto.cs
+++ Core/Application/DTOs/Project/CreateProjectDto.cs
@@ -6,7 +6,7 @@ public record CreateProjectDto
 {
     public string Name { get; init; } = string.Empty;
     public string? Description { get; init; }
-    public Visibility Visibility { get; init; } = Visibility.Private;
+    public string Visibility { get; init; } = string.Empty;
     public DateTime StartDate { get; init; }
     public DateTime? EndDate { get; init; }
 }
